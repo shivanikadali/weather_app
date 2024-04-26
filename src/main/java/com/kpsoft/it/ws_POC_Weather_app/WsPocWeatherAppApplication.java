06105d49a0c607d054dc87a3054d2805e5ac8aea
shivanikadali <shivanikadali2@gmail.com>
2024-04-26 17:05:48 +0530

    first commit

---
+++ src/main/java/com/kpsoft/it/ws_POC_Weather_app/WsPocWeatherAppApplication.java
@@ -0,0 +1,13 @@
+package com.kpsoft.it.ws_POC_Weather_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsPocWeatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsPocWeatherAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kpsoft/it/ws_POC_Weather_app/WsPocWeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpsoft.it.ws_POC_Weather_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsPocWeatherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
